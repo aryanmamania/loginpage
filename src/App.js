ee6b149481f1aadceabf7defa3ced99c22e8fad7
mamania0217 <aryanmamania17@gmail.com>
2022-11-09 14:28:46 +0530

    made a login page with reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import login from "./images/press-play.svg";
+import register from "./images/rocket.svg";
 
 function App() {
+  const [signupMode, setSignupMode] = useState("");
+
+  const signupBtn = () => {
+    setSignupMode("sign-up-mode");
+  };
+
+  const signinBtn = () => {
+    setSignupMode("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"app" + " " + signupMode}>
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form action="" className="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+          
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              
+
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" className="btn solid" />
+          </form>
+
+          <form action="" className="sign-up-form">
+            <h2 className="title">Sign up</h2>
+            <div className="input-field">
+             
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+            
+              <input type="Number" placeholder="Contact Number" />
+            </div>
+            <div className="input-field">
+            
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="input-field">
+               
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Sign up" className="btn solid" />
+          </form>
+        </div>
+      </div>
+
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>New here?</h3>
+            <p>Sign up now and Welcome to Vedspace Venture</p>
+            <button
+              className="btn transparent"
+              id="sign-up-btn"
+              onClick={signupBtn}
+            >
+              Sign up
+            </button>
+          </div>
+          <img src={register} className="image" alt="vedspace" />
+        </div>
+
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>Already Signed in ?</h3>
+            <p>Welcom back Login from here</p>
+            <button
+              className="btn transparent"
+              id="sign-in-btn"
+              onClick={signinBtn}
+            >
+              Sign in
+            </button>
+          </div>
+          <img src={login} className="image" alt="vedspace" />
+        </div>
+      </div>
     </div>
   );
 }
